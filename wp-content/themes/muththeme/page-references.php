7dba4a0b1fb571164bc525564e04a9255bbb0d7a
Bubo <bubomirxxx@gmail.com>
2016-07-08 16:15:19 +0200

    dynamikka na refernciach

--- wp-content/themes/muththeme/page-references.php
+++ wp-content/themes/muththeme/page-references.php
@@ -24,61 +24,67 @@ Template Name: References Page
 	<div class="muth-page-content">
         <div class="row">
         	<div class="muth-page-sub-title"> 
-        	
-        		<h2>Sub title</h2>
-
+        		<h2><?php if(get_field('title_web_references')): the_field('title_web_references'); endif; ?></h2>
         	</div> <!-- muth-page-title -->
-        	
-        	
-        	
         </div> <!-- row --> 
     </div> <!-- muth-page-content -->
 </section> <!-- muth-page-full-width muth-page-background-white -->
 
 <section class="muth-page-full-width muth-page-background-white">
-	<div class="muth-page-content">
+    <div class="muth-page-content">
         <div class="row">
-        	<div class = "muth-references-webs"> 
+            <div class = "muth-references-webs"> 
 
-        		<a href="" class = "muth-references-web-fix-size">
-	        		<img src="http://placehold.it/300x300">
-	        		<div class="muth-references-web-description">
-                        <div class="muth-reference-name">
-		        		    <h3 class="muth-reference-name-h3">Visko teepak a Visko teepak</h3>
-                        </div>
-						<span>poradenstvo, administracia , grafika, vyvoj, sprava webu, web aplikacia</span>
-	        		</div> <!-- muth-references-web-description -->
-        		</a> <!-- muth-references-web-fix-size -->
-        			
-        	</div><!--  muth-references-webs -->
-        	<div class = "muth-references-webs"> 
-        		 
-        		<div class = "muth-references-web-fix-size">
-        		<img src="http://placehold.it/300x300">
-        		<div class="muth-references-web-description">
-	        		<div class="muth-reference-name">
-                            <h3 class="muth-reference-name-h3">Visko teepak</h3>
-                        </div>
-					<span>poradenstvo, administracia , grafika, vyvoj, sprava webu, web aplikacia</span>
-        		</div>
-        		</div>
-        		
-        	</div><!--  muth-references-webs -->
-        	<div class = "muth-references-webs"> 
-        		 
-        		<img src="http://placehold.it/300x300">
-        		
-        	</div><!--  muth-references-webs -->
-		</div> <!-- row --> 
+<?php 
+if( have_rows('web_references') ):
+   
+    // loop through the rows of data
+    while ( have_rows('web_references') ) : the_row();
+
+        // display a sub field value
+        $picture = get_sub_field('thumbnail_web');
+        $project_name = get_sub_field('project_name');
+        $project_description = get_sub_field('project_description');
+
+        $alt_img = (isset($picture['alt']) && !empty($picture['alt']))?  'alt="'.$picture['alt'].'"' : __('');
+
+        if(isset($picture['url']) && !empty($picture['url'])):
+          $img = '<img src="'. $picture['url'] . '" '. $alt_img .'>';
+        else:
+           $img = '<img class="img-circle" src="http://placehold.it/300x300">';
+        endif;
+              
+       ?>
+
+       <div class = "muth-references-webs"> 
+            <a href="" class = "muth-references-web-fix-size">
+                <?php echo $img; ?>
+                <div class="muth-references-web-description">
+                    <div class="muth-reference-name">
+                        <h3 class="muth-reference-name-h3"><?php echo $project_name; ?></h3>
+                    </div>
+                    <span><?php echo $project_description; ?></span>
+                </div> <!-- muth-references-web-description -->
+            </a> <!-- muth-references-web-fix-size -->
+                
+        </div><!--  muth-references-webs -->
+<?php   
+    endwhile;
+else :
+    // no rows found
+endif;?>
+
+    </div> <!-- row --> 
     </div> <!-- muth-page-content -->
 </section> <!-- muth-page-full-width muth-page-background-white -->
+
 
 <section class="muth-page-full-width muth-page-background-gray">
 	<div class="muth-page-content">
         <div class="row">
         	<div class="muth-page-sub-title"> 
         	
-        		<h2>Sub title</h2>
+        		<h2><?php if(get_field('title_outsoarsing_references')): the_field('title_outsoarsing_references'); endif; ?></h2>
 
         	</div> <!-- muth-page-title -->
         	
@@ -106,7 +112,7 @@ Template Name: References Page
         <div class="row">
         	<div class="muth-page-sub-title"> 
         	
-        		<h2>Sub title</h2>
+        		<h2><?php if(get_field('title_shop_references')): the_field('title_shop_references'); endif; ?></h2>
 
         	</div> <!-- muth-page-title -->
         	
